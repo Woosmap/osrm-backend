4e4bbbfcd4bfcce3e4b967fe9edda90e548ec630
monteirodom <dmonteiro@webgeoservices.com>
2021-07-29 17:40:06 +0200

    Integrate boost tests on the test suite

--- test.sh
+++ test.sh
@@ -3,6 +3,42 @@
 set -e
 
 cd build
+
+echo "BOOST tests ..."
+cmake ..
+make tests
+cd unit_tests
+echo "Starting contractor-tests"
+./contractor-tests
+echo "Starting customizer-tests"
+./customizer-tests
+echo "Starting engine-tests"
+./engine-tests
+echo "Starting extractor-tests"
+./extractor-tests
+echo "Starting library-contract-tests"
+./library-contract-tests
+echo "Starting library-customize-tests"
+./library-customize-tests
+echo "Starting library-extract-tests"
+./library-extract-tests
+echo "Starting library-partition-tests"
+./library-partition-tests
+echo "Starting library-tests"
+./library-tests
+echo "Starting partitioner-tests"
+./partitioner-tests
+echo "Starting server-tests"
+./server-tests
+echo "Starting storage-tests"
+./storage-tests
+echo "Starting updater-tests"
+./updater-tests
+echo "Starting util-tests"
+./util-tests
+cd ..
+
+echo "Cucumber tests ..."
 cmake ..
 cmake --build .
 cmake --build . --target install
