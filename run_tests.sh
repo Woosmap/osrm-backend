81193a17814e0628b99db8a68189d88d6942f592
cmillett <cmillett@webgeoservices.com>
2021-08-02 11:26:46 +0200

    Fix build Use docker-compose instead of docker compose

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-docker compose -f docker-compose-test.yml build osrm-backend-test
-docker compose -f docker-compose-test.yml run --rm osrm-backend-test
+docker-compose -f docker-compose-test.yml build osrm-backend-test
+docker-compose -f docker-compose-test.yml run --rm osrm-backend-test
 
