991ee24618fc832a1c98b0860d0735ed8bec33ef
Sebastien Pro <spro@webgeoservices.com>
2021-10-04 14:47:52 +0200

    Add github action to deploy pr versions

--- deploy.sh
+++ deploy.sh
@@ -3,29 +3,21 @@ set -e
 # push osrm lib on gemfury and osrm-backend-wgs to ECR
 
 ECR_REPOSITORY=${1}
+VERSION=${2}
 BRANCH=${GITHUB_REF#refs/heads/}
 echo "Deploying version ${VERSION} if Master, we are ${BRANCH}"
 
-if [ -z ${GEMFURY_PUSH_TOKEN} ]; then
-    echo "Missing or blank GEMFURY_PUSH_TOKEN variable."
-    exit -1
-elif [ -z "${ECR_REPOSITORY}" ]; then
+if [ -z "${ECR_REPOSITORY}" ]; then
     echo "Missing ECR_REPOSITORY parameter."
-    echo "usage: deploy.sh <ECR_REPOSITORY>"
+    echo "usage: deploy.sh <ECR_REPOSITORY> <VERSION>"
     exit 255]
 fi
 
 if [ "${BRANCH}" ==  "master" ]; then
-
-    PACKAGE_FILE_NAME=osrm-wgs-${VERSION}
-
-    cd ./out/build
-
-    curl -F package=@./${PACKAGE_FILE_NAME}.deb https://${GEMFURY_PUSH_TOKEN}@push.fury.io/${GEMFURY_USERNAME}/
-
-    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ECR_REPOSITORY}
-    docker tag osrm-backend-wgs "${ECR_REPOSITORY}:${BRANCH}"
-    docker push "${ECR_REPOSITORY}:${BRANCH}"
+    echo "Deploy New Release"
+    #aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ECR_REPOSITORY}
+    #docker tag osrm-backend-wgs "${ECR_REPOSITORY}:${VERSION}"
+    #docker push "${ECR_REPOSITORY}:${VERSION}"
 else
     echo "Branch is not master."
 fi
